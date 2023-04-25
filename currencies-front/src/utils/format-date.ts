f27a112018a9eb5fa53b05211b2621ccf90e4edf
Vladislav Lanskikh <vladislav_lanskikh@epam.com>
2023-04-25 23:14:02 +0200

    Refactored code

--- currencies-front/src/utils/format-date.ts
+++ currencies-front/src/utils/format-date.ts
@@ -2,8 +2,8 @@ export const formatDate = (date: Date): string => {
   const year = date.getFullYear()
   const month = date.toLocaleString('default', { month: 'short' })
   const day = date.getDate()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-  const seconds = date.getSeconds()
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
   return `${year} ${month} ${day} ${hours}:${minutes}:${seconds}`
 }
